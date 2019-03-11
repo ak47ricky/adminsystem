4eb8a504a75e6bb69dbc9468e1bd7180277c6055
Ricky <ak47ricky24@gmail.com>
2019-03-11 17:35:22 +0800

    檔案上傳雛形

---
+++ js/buttonjs.js
@@ -0,0 +1,7 @@
+﻿$("#west").html(
+    "<div>" +
+    "<div style=\"padding:5px\">" +
+    "<a href=\"main.aspx\" class=\"easyui-linkbutton\" style=\"width:100%;height:60px\">上傳檔案管理</a>" +
+    "</div>" +
+    "</div>" 
+);
